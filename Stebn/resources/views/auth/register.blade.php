ab6fd9a63acfc8159e99bd7b330a0169feab790b
ahmedyahya94 <ahmedyahya_94@hotmail.com>
2015-03-20 23:04:41 +0200

    Done with my task #16 and finished #15

--- Stebn/resources/views/auth/register.blade.php
+++ Stebn/resources/views/auth/register.blade.php
@@ -7,7 +7,7 @@
 			<div class="panel panel-default">
 				<div class="panel-heading">Register</div>
 				<div class="panel-body">
-					@if (count($errors) > 0)
+			        @if (count($errors) > 0)
 						<div class="alert alert-danger">
 							<strong>Whoops!</strong> There were some problems with your input.<br><br>
 							<ul>
@@ -18,7 +18,7 @@
 						</div>
 					@endif
 
-					<form class="form-horizontal" role="form" method="POST" action="{{ url('/auth/register') }}">
+					<form class="form-horizontal" role="form" method="POST" action="/auth/register">
 						<input type="hidden" name="_token" value="{{ csrf_token() }}">
 
 						<div class="form-group">
--- Stebn/resources/views/welcome.blade.php
+++ Stebn/resources/views/welcome.blade.php
@@ -1,7 +1,6 @@
+@extends('app')
 <html>
 	<head>
-		<title>Laravel</title>
-		
 		<link href='//fonts.googleapis.com/css?family=Lato:100' rel='stylesheet' type='text/css'>
 
 		<style>
@@ -40,7 +39,7 @@
 	<body>
 		<div class="container">
 			<div class="content">
-				<div class="title">Laravel 5</div>
+				<div class="title">Stebn</div>
 				<div class="quote">{{ Inspiring::quote() }}</div>
 			</div>
 		</div>
